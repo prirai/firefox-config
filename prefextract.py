08854091d12c3eebfbe30b3f186577be78f9437b
Priyanshu Kumar Rai <58476539+prirai@users.noreply.github.com>
2023-09-26 22:56:27 +0530

    Delete prefextract.py

--- prefextract.py
+++
@@ -1,7 +0,0 @@
-with open('user.js', 'r') as f:
-    file_content = f.read()
-
-keys = [line.split('=')[0] for line in file_content.split('\n') if '=' in line]
-
-with open('keys.txt', 'w') as f:
-    f.write('\n'.join(keys))
